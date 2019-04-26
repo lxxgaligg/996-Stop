b3358dbf7db030a0a00cc6825bc5d7893c67d146
lxxgaligg <1459561964@qq.com>
2019-04-26 21:35:29 +0800

    第一次试验

---
+++ resources/views/Home/Index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>996Stop 拒绝996!吐槽发泄社区!</title>
+	<script src="/layui-v2.4.5/layui/layui.js"></script>
+	<link rel="stylesheet" href="/layui-v2.4.5/layui/css/layui.css">
+	<link rel="stylesheet" href="/layui-v2.4.5/layui/css/layui.mobile.css">
+</head>
+<body>
+	dadsadwda
+</body>
+</html>
